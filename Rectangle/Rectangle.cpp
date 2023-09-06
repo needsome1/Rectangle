d12e5a507ad27cc0f06c4a326fe655b743ba4455
Edgar Lavrinenko <destrxlarie@gmail.com>
2023-09-07 00:10:45 +0300

    Добавьте файлы проекта.

---
+++ Rectangle/Rectangle.cpp
@@ -0,0 +1,26 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Enter weight of rectangle" << endl;
+    int weight;
+    cin >> weight;
+    cout << "Enter height of rectangle" << endl;
+    int height;
+    cin >> height;
+    cout << "Choose symbole" << endl;
+    char symbol;
+    cin >> symbol;
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = 1; j <=weight; j++)
+        {
+            cout << symbol;
+            
+        }
+        cout << endl;
+    }
+}
+
